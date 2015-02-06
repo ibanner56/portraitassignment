cb1ca9603361c578d0ce4674724ae34ec65a71b9
Isaac <isb4190@rit.edu>
2015-02-06 10:51:56 -0500

    Initial commit - hover works, click works. Need to move images to a separate directive and get animations working.

---
+++ script/portrait.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Isaac on 2/4/2015.
+ */
+
+"use strict";
+
+(function() {
+    var app = angular.module('portraitPage', []);
+    app.controller('PortraitController', ['$scope', '$http', function($scope, $http) {
+        $scope.bios = [];
+        $scope.activeBio = 0;
+
+        $http.get('./bios.json').success(function(data) {
+            $scope.bios = data;
+            console.log($scope.bios);
+        });
+
+        $scope.show = function(index) {
+            return (index == $scope.activeBio);
+        }
+
+        $scope.set = function(index) {
+            $scope.activeBio = index;
+        }
+
+    }]);
+})();

9bfbe65bdf1ea3781c12483851270c7298f965de
Isaac <isb4190@rit.edu>
2015-02-16 22:53:19 -0500

    Lots of changes - added the image switch, got some of the images from Dionna.

--- script/portrait.js
+++ script/portrait.js
@@ -6,21 +6,40 @@
 
 (function() {
     var app = angular.module('portraitPage', ['ngAnimate']);
-    app.controller('PortraitController', ['$scope', '$http', function($scope, $http) {
+    app.controller('PortraitController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
         $scope.bios = [];
         $scope.activeBio = 0;
+        $scope.activeImage = 0;
+        $scope.rootIndex = 0;
 
         $http.get('./bios.json').success(function(data) {
             $scope.bios = data;
             console.log($scope.bios);
         });
 
-        $scope.show = function(index) {
+        $scope.setBio = function(index) {
+            $scope.activeBio = index;
+        };
+
+        $scope.showBio = function(index) {
             return (index == $scope.activeBio);
-        }
+        };
+
+        $scope.setImg = function(index) {
+            $scope.activeImage = index;
+        };
+
+        $scope.showImg = function(index) {
+            return (index == $scope.activeImage);
+        };
 
         $scope.set = function(index) {
             $scope.activeBio = index;
+            $scope.setImg(index);
+            $timeout(function() {
+                $scope.setImg($scope.rootIndex);
+                $scope.setBio($scope.rootIndex);
+            }, 5000);
         }
 
     }]);
